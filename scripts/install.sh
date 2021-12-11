6f8e1932023b74528854937930a21cea2db9a3f3
Axel Seemann <kummeraxel@gmail.com>
2021-12-11 22:24:24 +0100

    Fix setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 docker-compose build
-docker-compose run --rm -w /var/www php composer install
+docker-compose run --rm -u $(id -u) -w /var/www php composer install
 
 tar -xzf data/fileadmin.tar.gz --directory persistent
 
-docker-compose run --rm php bash -c "chown :www-data fileadmin -R && chmod g+w fileadmin -R"
+docker-compose run --rm php bash -c "chown :www-data fileadmin typo3temp -R && chmod g+w fileadmin typo3temp -R"
 docker-compose up -d
 
