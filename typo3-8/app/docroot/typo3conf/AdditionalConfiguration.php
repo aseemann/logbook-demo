ce8fbb609c6b4fa38636538735799a1f9fffb0df
Axel Seemann <kummeraxel@gmail.com>
2021-12-18 21:08:58 +0100

    Move typo3-8 in a subdir

--- typo3-8/app/docroot/typo3conf/AdditionalConfiguration.php
+++ typo3-8/app/docroot/typo3conf/AdditionalConfiguration.php
@@ -8,6 +8,7 @@ if (false === is_array($GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration']
 
 $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][\TYPO3\CMS\Core\Log\LogLevel::DEBUG]
 [\ASeemann\PhpLiveLog\Connector\TYPO3\LogWriter::class] = [
+    'ignorePattern' => '.*Frontend.*'
 ];
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_div.php']['devLog'] = [];
